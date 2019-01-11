4eb801dc30817b6891499daad0b66aff0c375039
Diego Morais <diegosiao@gmail.com>
2019-01-11 11:23:07 -0300

    Cleaning the exported main functions signature

--- www/cordova-plugin-android-sms-retriever.js
+++ www/cordova-plugin-android-sms-retriever.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.onSmsReceived = function(args, success, error) {
-    exec(success, error, "CordovaPluginAndroidSmsRetriever", "onSmsReceived", args);
+exports.onSmsReceived = function(success, error) {
+    exec(success, error, "CordovaPluginAndroidSmsRetriever", "onSmsReceived", []);
 }
 
-exports.getAppHash = function(args, success, error) {
-    exec(success, error, "CordovaPluginAndroidSmsRetriever", "getAppHash", args);
+exports.getAppHash = function(success, error) {
+    exec(success, error, "CordovaPluginAndroidSmsRetriever", "getAppHash", []);
 }
